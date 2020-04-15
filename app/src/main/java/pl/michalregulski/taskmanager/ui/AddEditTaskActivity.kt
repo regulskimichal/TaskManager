f809d2b446c8e9182b74dcb3e9df27a1813703bf
Michał Regulski <regulskimichal@outlook.com>
2020-04-15 23:43:34 +0200

    Keyboard fixes

--- app/src/main/java/pl/michalregulski/taskmanager/ui/AddEditTaskActivity.kt
+++ app/src/main/java/pl/michalregulski/taskmanager/ui/AddEditTaskActivity.kt
@@ -5,6 +5,7 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import android.text.format.DateFormat
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +14,6 @@ import androidx.core.content.getSystemService
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import pl.michalregulski.taskmanager.R
 import pl.michalregulski.taskmanager.databinding.ActivityAddEditTaskBinding
 import pl.michalregulski.taskmanager.model.TaskType
 import pl.michalregulski.taskmanager.utils.update
@@ -38,10 +38,10 @@ class AddEditTaskActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupSupportActionBar(binding.toolbar)
         setupFloatingActionButton(binding.fab)
-        setupFocusListeners(binding.root, binding.content.root)
+        setupFocusListeners(binding.content.addEditTaskCL, binding.content.taskTypeET)
         setupDateTimePicker(binding.content.dueDateET)
         setupTaskTypePicker(binding.content.taskTypeET)
-        prepareData()
+        prepareData(binding.content.titleET)
     }
 
     private fun setupSupportActionBar(toolbar: Toolbar) {
@@ -59,18 +59,20 @@ class AddEditTaskActivity : AppCompatActivity() {
         }
     }
 
-    private fun setupFocusListeners(root: View, content: View) {
-        content.isSoundEffectsEnabled = false
+    private fun setupFocusListeners(layout: ViewGroup, vararg views: View) {
+        layout.isSoundEffectsEnabled = false
 
-        root.viewTreeObserver.addOnGlobalFocusChangeListener { oldFocusView, newFocusView ->
-            if (newFocusView == null || (newFocusView != oldFocusView && !(newFocusView.id == R.id.titleET || newFocusView.id == R.id.descriptionET))) {
-                // https://medium.com/@rmirabelle/the-android-sdk-is-the-worst-thing-on-earth-c3aaebbd2e6d
-                // Where is something like Keyboard.hide()!?, why do I need to care about window
-                // and stupid flag which is not even an Enum...
-                val imm = getSystemService<InputMethodManager>()
-                imm?.hideSoftInputFromWindow(
-                    newFocusView?.windowToken ?: oldFocusView?.windowToken ?: root.windowToken, 0
-                )
+        layout.setOnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                hideKeyboard(view)
+            }
+        }
+
+        views.forEach {
+            it.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus) {
+                    hideKeyboard(view)
+                }
             }
         }
     }
@@ -78,7 +80,7 @@ class AddEditTaskActivity : AppCompatActivity() {
     private fun setupDateTimePicker(editText: TextInputEditText) {
         editText.showSoftInputOnFocus = false
 
-        editText.setOnFocusChangeListener { v, hasFocus ->
+        editText.setOnFocusChangeListener { view, hasFocus ->
             val task = viewModel.task.get()
             val time: LocalTime = if (task.id != 0L) task.dueDate.toLocalTime() else LocalTime.now()
             val date: LocalDate = if (task.id != 0L) task.dueDate.toLocalDate() else LocalDate.now()
@@ -106,6 +108,7 @@ class AddEditTaskActivity : AppCompatActivity() {
                 }
 
             if (hasFocus) {
+                hideKeyboard(view)
                 DatePickerDialog(
                     this,
                     onDateSetListener,
@@ -113,7 +116,7 @@ class AddEditTaskActivity : AppCompatActivity() {
                     date.monthValue - 1,
                     date.dayOfMonth
                 ).show()
-                v.clearFocus()
+                view.clearFocus()
             }
         }
     }
@@ -124,14 +127,23 @@ class AddEditTaskActivity : AppCompatActivity() {
         taskTypeET.setAdapter(TaskTypeAdapter(this))
     }
 
-    private fun prepareData() {
+    private fun prepareData(editText: TextInputEditText) {
         val id: Long? = intent.getSerializableExtra(EXTRA_ID) as Long?
         if (id != null) {
             viewModel.setTask(id)
         } else {
             val taskType: TaskType = intent.getSerializableExtra(EXTRA_TASK_TYPE) as TaskType
             viewModel.task.update { type = taskType }
+            editText.requestFocus()
         }
+    }
+
+    // https://medium.com/@rmirabelle/the-android-sdk-is-the-worst-thing-on-earth-c3aaebbd2e6d
+    // Where is something like Keyboard.hide()!?, why do I need to care about window
+    // and stupid flag which is not even an Enum...
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService<InputMethodManager>()
+        imm?.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onSupportNavigateUp(): Boolean {
